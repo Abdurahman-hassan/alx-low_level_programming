4d6683b11d6aeb461d74ffaa5ce8b7e2664a761b
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-08-10 23:44:55 +0000

    fix linting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,14 @@
 
 /**
   * main - Entry point
-  * 
+  *
   * Return: Always 0 (Success)
   */
-
 int main(void)
 {
 	char msg[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(msg);
 	return (0);
 }
+
