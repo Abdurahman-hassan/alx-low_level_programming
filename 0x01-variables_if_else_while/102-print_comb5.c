8c5932021f5153899ecdaec72a1accff0fd04af4
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-08-12 02:53:43 +0000

    fix linting

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	unsigned int first, second;
+
 	for (first = 0; first <= 99  ; first++)
 	{
 		for (second = first + 1; second <= 99; second++)
@@ -18,8 +19,8 @@ int main(void)
 
 			putchar ((second / 10) + '0');
 			putchar ((second % 10) + '0');
-			
-			if (first != 98 || second != 99 )
+
+			if (first != 98 || second != 99)
 			{
 				putchar(',');
 				putchar(' ');
