eaa25eb8f214b9b2e6ccd262b3cb62504f299a81
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-08-12 01:02:14 +0000

    program that prints all possible combinations of single-digit numbers, Numbers must be separated by ,followed by a space, Numbers should be printed in ascending order, can only use the putchar function (every other function (printf, puts, etc…) is forbidden)

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+
+/**
+  * main - the Entry point
+  *
+  * Return: 0 Always (success)
+  */
+int main(void)
+{
+	unsigned int n = 0;
+
+	while (n < 10)
+	{
+		/**
+		  * conver int to char
+		  * '0' is 48 in ASCII, so 2 + 48 == 50 that's equal to '2' in ASCI
+		  */
+
+		putchar(n + '0');
+		if (n == 9)
+			break;
+		putchar(',');
+		putchar(' ');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
