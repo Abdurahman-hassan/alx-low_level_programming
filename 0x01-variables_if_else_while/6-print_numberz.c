1d508487ca0656b76fe9e1c7af682eb2af5880ad
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-08-12 00:10:27 +0000

    program that prints all single digit numbers of base 10 starting from 0, followed by a new line, not allowed to use any variable of type char, only use the putchar twice

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+
+/**
+  * main - the Entry point
+  *
+  * Return: 0 Always (success)
+  */
+int main(void)
+{
+	unsigned int n;
+
+	while (n < 10)
+	{
+		/**
+		  * conver int to char
+		  * '0' is 48 in ASCII, so 2 + 48 == 50 that's equal to '2' in ASCI
+		  */
+
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
