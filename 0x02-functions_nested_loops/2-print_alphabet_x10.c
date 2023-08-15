6806221f64a673c69c45e08dedbde486987f25f9
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-08-15 23:58:49 +0000

    fix linting

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 # include "main.h"
 
 /**
-  * main - prints 10 times the alphabet, in lowercase,
+  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
   * followed by a new line.
   *
   * Return - 0 if success
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 	int i;
 	char c;
 
-	for (i = 0; i< 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
