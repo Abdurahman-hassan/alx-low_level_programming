50eaec0c998d7ff110853778d42ee78a51db3d47
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-08-16 02:26:01 +0000

    fix linting

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-  * print_to_98 - prints all natural numbers from n to 98, 
+  * print_to_98 - prints all natural numbers from n to 98
   * followed by a new line.
   *
   * @n: the checker number
@@ -11,24 +11,23 @@
   */
 void print_to_98(int n)
 {
-    if (n <= 98)
-    {
-        for (; n <= 98; n++)
-        {
-            printf("%d", n);
-            if (n != 98)
-                printf(", ");
-        }
-    }
-    else
-    {
-        for (; n >= 98; n--)
-        {
-            printf("%d", n);
-            if (n != 98)
-                printf(", ");
-        }
-    }
-    printf("\n");
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
 }
 
