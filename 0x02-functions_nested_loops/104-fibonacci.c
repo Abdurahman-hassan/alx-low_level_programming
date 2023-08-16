f89a44504f5c065621d283733405bcb4e59c3227
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-08-16 04:38:57 +0000

    fix the logic to match the output

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,15 +11,16 @@ int main(void)
 	j = 1;
 	k = 2;
 
-	printf("%lu", j);
+	printf("%lu, %lu", j, k);
 
-	for (i = 1; i < 91; i++)
+	for (i = 2; i < 91; i++)
 	{
-		printf(", %lu", k);
-		j = k + j;
-		k = j + k - j;
-	}
+		unsigned long int next = j + k;
 
+		printf(", %lu", next);
+		j = k;
+		k = next;
+	}
 	j1 = j / 1000000000;
 	j2 = j % 1000000000;
 	k1 = k / 1000000000;
@@ -27,21 +28,23 @@ int main(void)
 
 	for (i = 92; i < 99; i++)
 	{
-		printf(", %lu", k1 + k2 / 1000000000);
-		printf("%lu", k2 % 1000000000);
-		j1 = j1 + k1;
-		j2 = j2 + k2;
-		if (j2 > 1000000000)
+		unsigned long int next1, next2;
+
+		next1 = j1 + k1;
+		next2 = j2 + k2;
+		if (next2 > 1000000000)
 		{
-			j1++;
-			j2 = j2 % 1000000000;
+			next1 += 1;
+			next2 = next2 % 1000000000;
 		}
-		k1 = k1 + j1 - k1;
-		k2 = k2 + j2 - k2;
+		printf(", %lu%lu", next1, next2);
+		j1 = k1;
+		j2 = k2;
+		k1 = next1;
+		k2 = next2;
 	}
 
 	printf("\n");
-
 	return (0);
 }
 
