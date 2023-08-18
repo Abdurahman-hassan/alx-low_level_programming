596e364516eb4b429e386efd2b4c432d25fb8505
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-08-18 21:38:39 +0000

    retun to the special case

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,28 +1,29 @@
 #include "main.h"
 
 /**
- * print_number - Print an integer
- *
- * @n: The integer to be printed
- * Return: void
- */
+  * print_number - to print the integer numbers
+  * @n: is the number to check
+  * Return: the integer
+  */
 void print_number(int n)
 {
-	/* Handle negative numbers by printing '-' and making the number positive */
-	if (n < 0)
-	{	
-		n = -n;
-		_putchar('-');
-	}
-	else
+	/* Handle the special edge case for the smallest negative integer */
+	if (n == -2147483648)
 	{
-		n = n;
+		_putchar('-');
+		_putchar('2');
+		n = 147483648;
 	}
 
-	/* Print the digits recursively */
+	/* If the number is negative, print '-' and use positive part */
+	else if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	/* Recursively print the number */
 	if (n / 10)
 		print_number(n / 10);
-
 	_putchar((n % 10) + '0');
 }
-
