e093488fe6eeff123dac5abbb9f6b8a911113c2a
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-08-18 03:19:49 +0000

    use math liberary

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,74 +1,39 @@
 #include <stdio.h>
+#include <math.h>
 
 /**
- * find_next_prime_factor - Find smallest prime factor of a number.
- * @num: The number to find the prime factor for.
- * @start: Starting divisor for the search.
- *
- * Return: The smallest prime factor or -1 if none found.
- */
-long find_next_prime_factor(long num, long start)
-{
-	long divisor = start;
-
-	while (divisor <= num)
-	{
-		if (num % divisor == 0)
-			return (divisor);
-		divisor++;
-	}
-	return (-1);
-}
-
-/**
- * print_prime_factors - Print prime factors of a number.
- * @num: The number to print prime factors for.
- */
-void print_prime_factors(long num)
-{
-	long divisor = 2;
-	int factorCount = 0;
-	int printedFactors = 0;
-	long original_num = num;
-
-	while (num != 1)
-	{
-		divisor = find_next_prime_factor(num, divisor);
-		factorCount++;
-		num /= divisor;
-	}
-
-	divisor = 2;
-	num = original_num;
-	printf("The prime factors of %ld are ", num);
-
-	while (num != 1)
-	{
-		divisor = find_next_prime_factor(num, divisor);
-		if (printedFactors > 0)
-		{
-			if (printedFactors + 1 == factorCount)
-				printf(", and ");
-			else
-				printf(", ");
-		}
-		printf("%ld", divisor);
-		printedFactors++;
-		num /= divisor;
-	}
-	printf("\n");
-}
-
-/**
- * main - Entry point of the program.
- *
- * Return: 0 on success.
+ * main - Finds and prints the largest prime factor of the number 612852475143.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	long num = 612852475143;
+	long int n = 612852475143;
+	long int maxPrime = -1; /* Initially set to -1 for validation. */
+	long int i;
 
-	print_prime_factors(num);
+	/* Removing all the even factors of 2. */
+	while (n % 2 == 0)
+	{
+		maxPrime = 2;
+		n /= 2;
+	}
+
+	/* Finding odd prime factors. */
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			maxPrime = i;
+			n /= i;
+		}
+	}
+
+	/* If n is a prime number greater than 2. */
+	if (n > 2)
+		maxPrime = n;
+
+	printf("%ld\n", maxPrime);
+
 	return (0);
 }
 
