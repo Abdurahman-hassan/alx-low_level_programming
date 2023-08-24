a4ff9a9fc567a705ebbd1f48deb3fafb672dff06
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-08-24 21:04:02 +0000

    fix linting

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
   * @s1: the first string
   * @s2: The second string
   * Return: an integer less than, equal to,
-  * or greater than zero if s1 is found, 
+  * or greater than zero if s1 is found
   * respectively, to be less than, to match, or be greater than s2.
   */
 int _strcmp(char *s1, char *s2)
