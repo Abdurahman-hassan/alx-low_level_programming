35b12cc3f04aef972f832f96f432a1b1ca444cbc
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-08-29 01:38:29 +0000

    fix the logic

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,12 +7,9 @@
   */
 void print_chessboard(char (*a)[8])
 {
-	int i, j, len = 0;
+	int i, j;
 
-	while (a[len])
-		len++;
-
-	for (i = 0; i < len; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
