228c9be8e6cf539a0dc46503a0482fdda30feee4
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-09-02 03:00:00 +0000

    fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,62 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
- * is_valid_integer - Check if a string represents a valid integer.
- * @input: String to check.
- * Return: 1 if the string represents a valid integer, 0 otherwise.
- */
-int is_valid_integer(char *input)
-{
-	int i;
-
-	for (i = 0; input[i] != '\0'; i++)
-	{
-		if (!isdigit(input[i]))
-		{
-			return (0);
-		}
-	}
-	return (1);
-}
-
-/**
- * calculate_coins - Calculate the minimum number of coins needed to
- * make change for a given amount of cents.
- * @cents: Amount of cents.
- * Return: The minimum number of coins needed.
- */
-int calculate_coins(int cents)
-{
-	int quarters, dimes, nickels, pennies, total;
-
-	quarters = cents / 25;
-	cents %= 25;
-	dimes = cents / 10;
-	cents %= 10;
-	nickels = cents / 5;
-	cents %= 5;
-	pennies = cents;
-
-	total = quarters + dimes + nickels + pennies;
-
-	return (total);
-}
-
-/**
- * main - The entry point of the program.
- * @argc: The number of arguments passed to the program,
- *        including the program's name.
- * @argv: An array of strings representing the arguments passed
- *        to the program. The first argument is the program's name.
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: 0 on success, or a non-zero value on error.
+ * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-	int cents, total;
-	char *input;
+	int amount, i, coin_count;
+	int coin_values[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
@@ -64,25 +20,24 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	input = argv[1];
+	amount = atoi(argv[1]);
+	coin_count = 0;
 
-	if (!is_valid_integer(input))
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	cents = atoi(input);
-
-	if (cents < 0)
+	if (amount < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	total = calculate_coins(cents);
+	for (i = 0; i < 5 && amount >= 0; i++)
+	{
+		while (amount >= coin_values[i])
+		{
+			coin_count++;
+			amount -= coin_values[i];
+		}
+	}
 
-	printf("%d\n", total);
-
+	printf("%d\n", coin_count);
 	return (0);
 }
