7ee77f37eac3bdd40cd89c92dc1fad90bfb57d31
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-09-02 02:46:37 +0000

    program that prints the minimum number of coins to make change for an amount of money, where cents is the amount of cents you need to give back, if the number of arguments passed to your program is not exactly 1, print Error, followed by a new line, and return 1, you should use atoi to parse the parameter passed to your program, If the number passed as the argument is negative, print 0, followed by a new line, You can use an unlimited number of coins of values 25, 10, 5, 2, and 1 cent.

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - The entry point of the program.
+ * @argc: The number of arguments passed to the program,
+ *        including the program's name.
+ * @argv: An array of strings representing the arguments passed
+ *        to the program. The first argument is the program's name.
+ *
+ * Return: 0 on success, or a non-zero value on error.
+ */
+int main(int argc, char *argv[])
+{
+	int cents, quarters, dimes, nickels, pennies, total;
+
+	/* Check if the number of arguments is correct */
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	/* Convert the argument to an integer */
+	cents = atoi(argv[1]);
+
+	/* If the number of cents is negative, print 0 and return */
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	/* Calculate the number of coins needed */
+	quarters = cents / 25;
+	cents %= 25;
+	dimes = cents / 10;
+	cents %= 10;
+	nickels = cents / 5;
+	cents %= 5;
+	pennies = cents;
+
+	/* The total number of coins is the sum of all the coins calculated */
+	total = quarters + dimes + nickels + pennies;
+
+	printf("%d\n", total);
+
+	return (0);
+}
