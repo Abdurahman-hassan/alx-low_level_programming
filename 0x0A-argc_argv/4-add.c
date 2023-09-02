c35ba6256f48d736b3ef94d7178ee0d003aedaf0
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-09-02 01:59:52 +0000

    program that adds positive numbers, Print the result, followed by a new line, If no number is passed to the program, print 0, followed by a new line, If one of the number contains symbols that are not digits, print Error, followed by a new line, and return 1, You can assume that numbers and the addition of all the numbers can be stored in an int

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,56 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include "main.h"
+
+/**
+  * is_number - this function check that str is a number
+  * @str: the string that will be checked
+  * return 0 if false and 1 if true
+  */
+int is_number(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+		{
+			return (0);
+		}
+
+		i++;
+	}
+
+	return (1);
+}
+
+/**
+ * main - The entry point of the program.
+ * @argc: The number of arguments passed to the program,
+ *        including the program's name.
+ * @argv: An array of strings representing the arguments passed
+ *        to the program. The first argument is the program's name.
+ *
+ * Return: 0 on success, or a non-zero value on error.
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (is_number(argv[i]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
