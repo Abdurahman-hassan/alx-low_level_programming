98b3a604a6213359fd4adf894e7c90c54c7548ad
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-09-08 02:44:59 +0000

    fix the logic

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,92 +1,139 @@
-#include "main.h"
 #include <stdlib.h>
-#include <ctype.h>
-
-/* Function prototypes */
-int is_only_digits(char *str);
-void print_number(unsigned long int n);
-void print_error(void);  /* Forward declaration of print_error */
+#include "main.h"
 
 /**
- * main - Entry point for the multiplication program.
- * @argc: The number of command-line arguments.
- * @argv: Array containing the program command-line arguments.
- *
- * Return: 0 on success, or exit with a status of 98 on error.
+ * is_digit - Check if a string consists only of digits.
+ * @s: The string to evaluate.
+ * Return: 1 if the string consists only of digits, 0 otherwise.
+ * Description: This function loops through each character of the string
+ * and checks if it's a digit or not.
  */
-int main(int argc, char *argv[])
+int is_digit(char *s)
 {
-	unsigned long int num1, num2, product;
-
-	/* Check if the number of arguments is correct */
-	if (argc != 3)
+	while (*s)
 	{
-		print_error();
-		exit(98);
-	}
-
-	/* Check if num1 and num2 contain only digits */
-	if (!is_only_digits(argv[1]) || !is_only_digits(argv[2]))
-	{
-		print_error();
-		exit(98);
-	}
-
-	/* Convert the string arguments to unsigned long integers and multiply */
-	num1 = strtoul(argv[1], NULL, 10);
-	num2 = strtoul(argv[2], NULL, 10);
-	product = num1 * num2;
-
-	/* Print the multiplication result */
-	print_number(product);
-	_putchar('\n');
-	return (0);
-}
-
-/**
- * is_only_digits - Verify if a string contains only digit characters.
- * @str: The string to check.
- *
- * Return: 1 if the string contains only digits, otherwise 0.
- */
-int is_only_digits(char *str)
-{
-	while (*str)
-	{
-		if (!isdigit(*str))
+		if (*s < '0' || *s > '9')
 			return (0);
-		str++;
+		s++;
 	}
 	return (1);
 }
 
 /**
- * print_number - Print an unsigned long integer to stdout.
- * @n: The number to be printed.
+ * _strlen - Compute the length of a string.
+ * @s: The string to measure.
  *
- * Description: This function recursively divides the number by 10 to
- *              determine each digit, which is then printed with _putchar.
+ * Return: The length of the string.
+ *
+ * Description: Loops through the string till the null terminator is found
+ * and returns the length.
  */
-void print_number(unsigned long int n)
+int _strlen(char *s)
 {
-	if (n / 10)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	int i = 0;
+
+	while (*s++)
+		i++;
+	return (i);
 }
 
 /**
- * print_error - Print an error message to stdout.
+ * errors - Print an error message and exit the program.
  *
- * Description: This function outputs "Error" followed by a newline.
+ * Description: This function prints "Error" and exits the program
+ * with a status code of 98.
  */
-void print_error(void)
+void errors(void)
 {
-	char *error = "Error";
-
-	while (*error)
-	{
-		_putchar(*error);
-		error++;
-	}
+	_putchar('E');
+	_putchar('r');
+	_putchar('r');
+	_putchar('o');
+	_putchar('r');
 	_putchar('\n');
+	exit(98);
+}
+
+/**
+ * multiply - Multiply two strings representing numbers.
+ * @s1: First number as a string.
+ * @s2: Second number as a string.
+ * @len1: Length of s1.
+ * @len2: Length of s2.
+ *
+ * Return: A pointer to an array holding the result.
+ *
+ * Description: This function multiplies two numbers represented as strings.
+ * It utilizes the classical multiplication method to compute the result.
+ */
+int *multiply(char *s1, char *s2, int len1, int len2)
+{
+	int i, j, carry, *result;
+	int len = len1 + len2;
+
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		errors();
+
+	for (i = 0; i < len; i++)
+		result[i] = 0;
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			int sum;
+
+			sum = (s1[i] - '0') * (s2[j] - '0') + result[i + j + 1] + carry;
+			result[i + j + 1] = sum % 10;
+			carry = sum / 10;
+		}
+		result[i + j + 1] += carry;
+	}
+
+	return (result);
+}
+
+/**
+ * main - Multiplies two positive numbers.
+ * @argc: The number of command line arguments.
+ * @argv: The command line arguments.
+ * Return: 0 if successful, or 1 if an error occurs.
+ * Description: The main function first checks the input arguments.
+ * If they're valid, it calls the multiply function and then
+ * displays the result.
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, *result, a = 0;
+
+	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+		errors();
+
+	s1 = argv[1];
+	s2 = argv[2];
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	result = multiply(s1, s2, len1, len2);
+	len = len1 + len2;
+
+	for (i = 0; i < len; i++)
+	{
+		if (result[i] != 0 || a != 0)
+		{
+			_putchar(result[i] + '0');
+			a = 1;
+		}
+	}
+
+	if (a == 0)
+		_putchar('0');
+
+	_putchar('\n');
+
+	free(result);
+	return (0);
 }
