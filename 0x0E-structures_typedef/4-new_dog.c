269d4f18a922f95ca8bf9b415d43bde3ca5e0b1b
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-09-12 02:09:58 +0000

    function that creates a new dog., Prototype: dog_t *new_dog(char *name, float age, char *owner);, You have to store a copy of name and owner, Return NULL if the function fails

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+  * new_dog - function that creates a new dog.
+  * @name: is the name of dog
+  * @age: the age of dog
+  * owner: the name of the owner of the dog
+  * Return: a pinter to a struct new_dog
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+
+	ptr = malloc(sizeof(dog_t));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	ptr->name = name;
+	(*ptr).owner = owner;
+	ptr->age = age;
+
+	return(ptr);
+
+}
