e4e88ece6f9d5f5d86b7d51b9a37f1877897a4d0
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-09-27 01:16:01 +0000

    fix the logic

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,84 +1,33 @@
 #include "lists.h"
 
 /**
- * detect_loop - Detects if there is a loop in the list.
- * @head: The head of the list.
+ * print_listint_safe - prints a linked list, safely
+ * @head: list of type listint_t to print
  *
- * Return: The start node of the loop or NULL.
- */
-const listint_t *detect_loop(const listint_t *head)
-{
-	/* Pointers for detecting loop using Floyd's Cycle-Finding algorithm */
-	const listint_t *slow = head, *fast = head;
-
-	/* To store the starting node of the detected loop, if present */
-	const listint_t *loop_start = NULL;
-
-	/* Traverse the list looking for a loop */
-	while (fast && fast->next)
-	{
-		slow = slow->next; /* Move the slow pointer one step */
-		fast = fast->next->next; /* Move the fast pointer two steps */
-
-		/* If both pointers meet, there's a loop */
-		if (slow == fast)
-		{
-			loop_start = slow; /* Save the meeting point */
-			break;
-		}
-	}
-
-	/* If a loop is detected, find its starting point */
-	if (loop_start)
-	{
-		slow = head; /* Reset slow pointer to the beginning of the list */
-		while (slow != fast)
-		{
-			fast = fast->next; /* Move fast pointer one step */
-			if (slow == fast)
-			{
-				loop_start = slow; /* Mark the loop start */
-				break;
-			}
-			slow = slow->next; /* Move slow pointer one step */
-		}
-	}
-
-	return (loop_start); /* Return the loop's starting node or NULL if no loop */
-}
-
-/**
- * print_listint_safe - Prints a listint_t list in a safe way.
- * @head: The head of the list.
- *
- * Return: The number of nodes in the list.
+ * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	/* Counter for the number of nodes printed */
-	size_t count = 0;
+	size_t count = 0;   /* Counter for the number of nodes printed */
+	/* To calculate the difference between current and next node addresses */
+	long int diff;
 
-	/* Detect loop in the list */
-	const listint_t *loop_start = detect_loop(head);
-
-	/* If list is empty, exit with an error code */
-	if (!head)
-		exit(98); /* Convention for a detected failure */
-
-	/* Print nodes until the loop's start, or until the list ends */
-	while (head != loop_start)
+	while (head)
 	{
+		diff = head - head->next;
+		count++;  /* Increment the node count */
 		printf("[%p] %d\n", (void *)head, head->n);
-		count++; /* Increment the node count */
-		head = head->next; /* Move to the next node */
+
+		if (diff > 0)
+		/* Move to the next node if progressing normally */
+			head = head->next; 
+		else
+		{
+			/* Detect the loop and print the loop's starting node */
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;  /* Exit the loop */
+		}
 	}
 
-	/* If a loop is detected, print the loop's starting node */
-	if (loop_start)
-	{
-		printf("-> [%p] %d\n", (void *)loop_start, loop_start->n);
-		count++; /* Increment the node count */
-	}
-
-	return (count); /* Return the total count of printed nodes */
+	return (count);  /* Return the total count of printed nodes */
 }
