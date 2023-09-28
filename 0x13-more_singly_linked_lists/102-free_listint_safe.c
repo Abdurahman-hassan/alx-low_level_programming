8344b36b0d73bd6e3d3e309feba5d2d387f27d45
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-09-28 01:52:21 +0000

    fix the logic

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,48 +1,67 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a linked list, safely
- * @head: list of type listint_t to print
- *
- * Return: number of nodes in the list
+ * find_loop_start - find the start node of loop using Floyd's algorithm
+ * @head: pointer to head of list
+ * Return: start node of loop or NULL
  */
-size_t print_listint_safe(const listint_t *head)
+listint_t *find_loop_start(listint_t *head)
 {
-	const listint_t *tortoise, *hare;
-	size_t count = 0; /* Counter for the number of nodes printed */
-
-	if (!head)
-		return (0);
+	listint_t *tortoise, *hare;
 
 	tortoise = hare = head;
 
-	/* Check if a cycle exists */
+	/* Detect a loop using the Floyd's cycle detection algorithm */
 	while (hare && hare->next)
 	{
-		if (count > 0 && tortoise == hare)
-			break;
-
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
 		tortoise = tortoise->next;
 		hare = hare->next->next;
-		count++;
 
-		/* If hare and tortoise meet, cycle detected */
-		if (tortoise == hare && count > 0)
+		if (tortoise == hare)
 		{
-			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
-			break;
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (hare);
 		}
 	}
 
-	/* If no cycle was detected, print remaining nodes */
-	while (count == 0 && tortoise)
+	return (NULL);
+}
+
+/**
+ * free_listint_safe - frees a listint_t list safely
+ * @h: pointer to a pointer to head of the list
+ *
+ * Return: the size of the list that was freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count = 0;
+	listint_t *loop_start, *next_node;
+
+	if (!h || !*h)
+		return (0);
+
+	loop_start = find_loop_start(*h);
+
+	while (*h && (loop_start != *h || count == 0))
 	{
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-		tortoise = tortoise->next;
+		next_node = (*h)->next;
+		free(*h);
+		*h = next_node;
 		count++;
+
+		/* If we've detected a loop, we want to stop */
+		/* once we circle back to the start of the loop. */
+		if (*h == loop_start && count != 0)
+			break;
 	}
 
+	*h = NULL; /* Set the head to NULL */
 	return (count);
 }
 
