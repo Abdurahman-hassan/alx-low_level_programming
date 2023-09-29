0f47d82be22f9849030cc84b24620486efaa679c
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-09-29 02:00:06 +0000

    fix linting

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * flip_bits - Returns the number of bits to flip to get from one number to another.
+ * flip_bits - Returns the number of bits to flip
+ * to get from one number to another.
  * @n: The first number.
  * @m: The second number.
  *
- * Return: The number of bits you would need to flip to get from one number to another.
+ * Return: The number of bits you would need to flip
+ * to get from one number to another.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
