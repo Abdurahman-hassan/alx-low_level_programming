c0e811b26083057ee29fd3ad8c2e6bb33d022462
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-10-03 01:33:45 +0000

    split into two functions

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,17 +1,85 @@
 #include "main.h"
 
-#include "main.h"
+/**
+ * copy_content - Copies content from one file to another.
+ * @source_fd: File descriptor for the source file.
+ * @dest_fd: File descriptor for the destination file.
+ * Return: 0 on success, -1 on failure.
+ */
+int copy_content(int source_fd, int dest_fd)
+{
+	char buffer[BUFSIZ];
+	ssize_t bytes_read;
+
+	while ((bytes_read = read(source_fd, buffer, BUFSIZ)) > 0)
+	{
+		if (write(dest_fd, buffer, bytes_read) != bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file\n");
+			return (-1);
+		}
+	}
+
+	if (bytes_read < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from source file\n");
+		return (-1);
+	}
+
+	return (0);
+}
 
 /**
- * main - copies the content of a file to another file
- * @argc: number of arguments passed to the program
- * @argv: argument list
- * Return: 0 on success, or one of the error codes on failure
+ * handle_files - Handles opening and closing of files.
+ * @src: Name of the source file.
+ * @dest: Name of the destination file.
+ * Return: 0 on success, or the appropriate error code.
+ */
+int handle_files(char *src, char *dest)
+{
+	int source_fd, dest_fd, close_status;
+
+	source_fd = open(src, O_RDONLY);
+	if (source_fd < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		return (98);
+	}
+
+	dest_fd = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (copy_content(source_fd, dest_fd) == -1)
+	{
+		close(source_fd);
+		return (99);
+	}
+
+	close_status = close(source_fd);
+	if (close_status < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source_fd);
+		return (100);
+	}
+
+	close_status = close(dest_fd);
+	if (close_status < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd);
+		return (100);
+	}
+
+	return (0);
+}
+
+/**
+ * main - Entry point of the program.
+ * Copies the content of a file to another file.
+ * @argc: number of arguments passed to the program.
+ * @argv: argument list.
+ * Return: 0 on success, or one of the error codes on failure.
  */
 int main(int argc, char *argv[])
 {
-	int source_fd, dest_fd, bytes_read, close_src_status, close_dest_status;
-	char buffer[BUFSIZ];
+	int status;
 
 	if (argc != 3)
 	{
@@ -19,41 +87,10 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	source_fd = open(argv[1], O_RDONLY);
-	if (source_fd < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	status = handle_files(argv[1], argv[2]);
 
-	dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((bytes_read = read(source_fd, buffer, BUFSIZ)) > 0)
-	{
-		if (dest_fd < 0 || write(dest_fd, buffer, bytes_read) != bytes_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(source_fd);
-			exit(99);
-		}
-	}
-
-	if (bytes_read < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	close_src_status = close(source_fd);
-	close_dest_status = close(dest_fd);
-
-	if (close_src_status < 0 || close_dest_status < 0)
-	{
-		if (close_src_status < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source_fd);
-		if (close_dest_status < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd);
-		exit(100);
-	}
+	if (status != 0)
+		exit(status);
 
 	return (0);
 }
