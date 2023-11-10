8db49f22b19c3db2d0adb6a3fcf39fee898403ec
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-10 01:34:08 +0000

    fix linting

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
-* size_t print_dlistint - prints all the elements of a dlistint_t list
-* @h: pointer to head of list
-* Return: number of nodes
-*/
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        ++count;
-        printf("%d\n", h->n);
-        h = h->next;
-    }
-    return (count);
+	while (h != NULL)
+	{
+		++count;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
 }
