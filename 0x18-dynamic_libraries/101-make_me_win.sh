556900dea4313fce25e9c153222783350d8b55e4
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-14 04:47:33 +0200

    fix the script

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Abdurahman-hassan/alx-low_level_programming/raw/main/0x18-dynamic_libraries/i_will_win
+wget -P /tmp https://github.com/Abdurahman-hassan/alx-low_level_programming/raw/main/0x18-dynamic_libraries/i_will_win.so
 export LD_PRELOAD=/tmp/i_will_win
