60267bd8120115c159a0e01baf62130921edda67
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-14 04:44:17 +0200

    fix the link of file, upload the right file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/Abdurahman-hassan/alx-low_level_programming/blob/main/0x18-dynamic_libraries/make_me_win.so
-export LD_PRELOAD=/tmp/make_me_win.so
+wget -P /tmp https://github.com/Abdurahman-hassan/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
