528bb519c7910848ac2ee3731a179df1709d5edb
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-14 03:58:07 +0200

    fix linting

--- 0x18-dynamic_libraries/libdynamic.c
+++ 0x18-dynamic_libraries/libdynamic.c
@@ -346,27 +346,37 @@ char *_strchr(char *s, char c)
 	return (NULL);
 }
 
-
-unsigned int _strspn(char *s, char *accept) {
+/**
+ * _strspn - function that gets the length of a prefix substring.
+ * The _strspn() function calculates the length (in bytes) of the initial
+ * segment of s which consists entirely of bytes in accept
+ * @s: the main string
+ * @accept: the string that will look for the first match string
+ * Return: the number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
 	unsigned int i, j, count = 0;
 
 	for (i = 0; s[i] != '\0'; i++) {
-		int found = 0;  // Flag to check if s[i] exists in accept
+		int found = 0;  /* Flag to check if s[i] exists in accept*/
 
-		for (j = 0; accept[j] != '\0'; j++) {
-			if (s[i] == accept[j]) {
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
 				found = 1;
 				count++;
-				break;  // No need to continue the inner loop once a match is found
+				break;  /* No need to continue the inner loop once a match is found*/
 			}
 		}
 
-		if (!found) {
-			break;  // Exit the loop if the character is not found in accept
-		}
+		if (!found)
+			break;  /* Exit the loop if the character is not found in accept*/
 	}
 
-	return count;
+	return (count);
 }
 
 /**
