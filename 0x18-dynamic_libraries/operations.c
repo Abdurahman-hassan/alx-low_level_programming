c03087beb989e572bbe708bcc4d32ff7cb6f983f
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-14 04:17:19 +0200

    add description to file

--- 0x18-dynamic_libraries/operations.c
+++ 0x18-dynamic_libraries/operations.c
@@ -1,20 +1,52 @@
 #include <stdio.h>
 
+/**
+ * add - function that adds two integers and returns the result
+ *
+ * @a: first number to add
+ * @b: second number to add
+ *
+ * Return: The result of the addition
+ */
 int add(int a, int b)
 {
 	return (a + b);
 }
 
+/**
+ * sub - function that subtracts two integers and returns the result
+ *
+ * @a: first number to subtract
+ * @b: second number to subtract
+ *
+ * Return: The result of the subtraction
+ */
 int sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * mul - function that multiplies two integers and returns the result
+ *
+ * @a: first number to multiply
+ * @b: second number to multiply
+ *
+ * Return: The result of the multiplication
+ */
 int mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * div - function that divides two integers and returns the result
+ *
+ * @a: first number to divide
+ * @b: second number to divide
+ *
+ * Return: The result of the division
+ */
 int div(int a, int b)
 {
 	if (b != 0)
@@ -23,6 +55,14 @@ int div(int a, int b)
 		return (0);
 }
 
+/**
+ * mod - function that returns the remainder of dividing two integers
+ *
+ * @a: first number to divide
+ * @b: second number to divide
+ *
+ * Return: The remainder of the division
+ */
 int mod(int a, int b)
 {
 	if (b != 0)
@@ -30,4 +70,3 @@ int mod(int a, int b)
 	else
 		return (0);
 }
-
