420dfde9888b71f9a5df7142e208e80fe1fa25cc
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-11-14 04:29:24 +0200

    fix script file, make it point to the right file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/liball.so
+wget -P /tmp/ https://github.com/Abdurahman-hassan/alx-low_level_programming/raw/master/0x18-dynamic_libraries/make_me_win.so
 export LD_PRELOAD=/tmp/liball.so
