d83b44ce2ab823343caf61eecde96c8f5c07e014
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-03-14 02:46:37 +0200

    fix linting

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,19 +7,19 @@
  * @value: value to search for
  * Return: index of value or -1 if not found
  */
+
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
 	if (array == NULL)
 		return (-1);
-
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[inc]);
 		if (array[i] == value)
 			return (i);
 	}
 	return (-1);
 }
-
